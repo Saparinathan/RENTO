7c7abf485ef3f0226dd013d2902a8e1e4508eea9
Saparinathan S <saparinathan2003@gmail.com>
2024-05-01 09:52:56 -0700

    Created RENTO

---
+++ script.js
@@ -0,0 +1,8 @@
+var sidenav = document.querySelector(".side-navbar")
+function ShowNavBar() {
+    sidenav.style.display = "block"
+    sidenav.style.top = "67px"
+}
+function CloseNavBar() {
+    sidenav.style.top = "-60%"
+}
